4a0da831da6e931f9e97466c2135a3d85db6ef4f
HalfXnso <alfonsogaitanperez@gmail.com>
2024-10-15 14:10:58 +0200

    klkl

--- Tema 3/Ejercicio1T3AD/src/ejercicio1t3ad/Ejercicio1T3AD.java
+++ Tema 3/Ejercicio1T3AD/src/ejercicio1t3ad/Ejercicio1T3AD.java
@@ -3,15 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplohibernate;
+package ejercicio1t3ad;
 
-import controlador.HibernateUtil;
+import Controlador.NewHibernateUtil;
+import Modelos.Socios;
 import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import modelo.Peliculas;
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -22,37 +22,36 @@ import org.hibernate.Transaction;
  *
  * @author AlfonsoGaitanPerez
  */
-public class EjemploHibernate {
+public class Ejercicio1T3AD {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
 
         Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
-        SessionFactory miConexion = HibernateUtil.getSessionFactory();
+        SessionFactory miConexion = NewHibernateUtil.getSessionFactory();
 
         BigDecimal precio = new BigDecimal(5.50);
 
         System.out.println(consultaPelicula(miConexion, 69));
         System.out.println("\n");
-        borraPeliculas(miConexion, 1288);
-        List<Peliculas> listaPeliculas = listaPeliculas(miConexion);
-        for (Iterator<Peliculas> iterator = listaPeliculas.iterator(); iterator.hasNext();) {
-            Peliculas next = iterator.next();
+        borraSocios(miConexion, 1288);
+        List<Socios> listaSocios = listaSocios(miConexion);
+        for (Iterator<Socios> iterator = listaSocios.iterator(); iterator.hasNext();) {
+            Socios next = iterator.next();
             System.out.println(next);
         }
 
-        
-        actualizaPeliculas(miConexion, new Peliculas(69, "Edgar y las babys", "Sexual", Integer.SIZE, precio));
         miConexion.close();
     }
 
-    public static void altaPelicula(SessionFactory miConexion, Peliculas miPelicula) {
+    public static void altaPelicula(SessionFactory miConexion, Socios miSocio) {
         Session sesion = miConexion.openSession();
         Transaction transaccion = sesion.beginTransaction();
 
-        sesion.save(miPelicula);
+        sesion.save(miSocio);
         transaccion.commit();
 
         sesion.close();
@@ -60,50 +59,50 @@ public class EjemploHibernate {
 
     }
 
-    public static Peliculas consultaPelicula(SessionFactory miConexion, int codPelicula) {
+    public static Socios consultaPelicula(SessionFactory miConexion, int codSocio) {
         Session sesion = miConexion.openSession();
         Transaction transaccion = sesion.beginTransaction();
 
-        Peliculas miPelicula = (Peliculas) sesion.get(Peliculas.class, codPelicula);
+        Socios miSocio = (Socios) sesion.get(Socios.class, codSocio);
 
         transaccion.commit();
 
         sesion.close();
         System.out.println("Los semáforos son como las pastillas: Rojas,verdes y amarillas");
-        return miPelicula;
+        return miSocio;
     }
 
-    public static List<Peliculas> listaPeliculas(SessionFactory miConexion) {
+    public static List<Socios> listaSocios(SessionFactory miConexion) {
         Session sesion = miConexion.openSession();
         Transaction transaccion = sesion.beginTransaction();
 
-        Query registros = sesion.createQuery("from Peliculas");
-        List<Peliculas> lista = registros.list();
+        Query registros = sesion.createQuery("from Socios");
+        List<Socios> lista = registros.list();
 
         transaccion.commit();
         sesion.close();
         return lista;
     }
 
-    public static void borraPeliculas(SessionFactory miConexion, int codPelicula) {
+    public static void borraSocios(SessionFactory miConexion, int codSocio) {
         Session sesion = miConexion.openSession();
         Transaction transaccion = sesion.beginTransaction();
 
-        Peliculas miPelicula = new Peliculas();
-        miPelicula.setCodpelicula(codPelicula);
+        Socios miSocio = new Socios();
+        miSocio.setCodsocio(codSocio);
 
-        sesion.delete(miPelicula);
+        sesion.delete(miSocio);
         System.out.println("Pelicula eliminada");
 
         transaccion.commit();
         sesion.close();
     }
 
-    public static void actualizaPeliculas(SessionFactory miConexion, Peliculas miPelicula) {
+    public static void actualizaSocios(SessionFactory miConexion, Socios miSocio) {
         Session sesion = miConexion.openSession();
         Transaction transaccion = sesion.beginTransaction();
 
-        sesion.update(miPelicula);
+        sesion.update(miSocio);
         transaccion.commit();
         sesion.close();
 
